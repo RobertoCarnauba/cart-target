3d01d6adb23c9272c06710674cf88ef0aa2f4532
RobertoCarnauba <betocarnauba@hotmail.com>
2021-09-03 12:18:30 -0300

    startando o projeto...

---
+++ src/main/java/com/carnauba/CartTargetApplication.java
@@ -0,0 +1,13 @@
+package com.carnauba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartTargetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartTargetApplication.class, args);
+	}
+
+}
